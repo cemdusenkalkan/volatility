a6b6990202f3d35166c2d96365820a31a6d8a321
cemd <99793829+cemdusenkalkan@users.noreply.github.com>
2023-12-03 14:30:16 +0300

    Added basic functions to load and read csv file, it doesn't include exceptions, more details in code comments.

---
+++ src/config.py
@@ -0,0 +1,24 @@
+CSV_FILE_PATH = 'your/path/to/data.csv'
+
+WINDOW_SIZE = 5
+DATE_COLUMN = 'Date'
+PRICE_COLUMN = 'Close'
+RETURN_COLUMN = 'Daily_Return'
+
+LEARNING_RATE = 0.01
+N_EPOCHS = 100
+BATCH_SIZE = 32
+
+TEST_SPLIT_RATIO = 0.2
+VALIDATION_SPLIT_RATIO = 0.1
+
+FEATURE_COLUMNS = ['Open', 'High', 'Low', 'Close', 'Volume']
+TARGET_COLUMN = 'Future Volatility'
+
+RANDOM_SEED = 42
+
+MODEL_SAVE_PATH = 'decide/yourself/the/path'
+
+HISTORY_WINDOW = 31
+
+RESULTS_SAVE_PATH = 'decide/yourself/for/paths/logs'
