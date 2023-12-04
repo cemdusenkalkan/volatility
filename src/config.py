b5ba901c4aa7e29f08f84798f75881e855365740
cemd <99793829+cemdusenkalkan@users.noreply.github.com>
2023-12-04 15:48:20 +0300

    Added configurations,
    Model selection is still empty. Can add more configs but not now.

--- src/config.py
+++ src/config.py
@@ -12,8 +12,13 @@ BATCH_SIZE = 32
 TEST_SPLIT_RATIO = 0.2
 VALIDATION_SPLIT_RATIO = 0.1
 
-FEATURE_COLUMNS = [ 'Open', 'High', 'Low', 'Close', 'Volume']
+FEATURE_COLUMNS = ['Open', 'High', 'Low', 'Close', 'Volume']
 TARGET_COLUMN = 'Future Volatility'
+
+RF_N_ESTIMATORS = 100
+RF_MAX_DEPTH = None
+RF_MIN_SAMPLES_SPLIT = 2
+RF_MIN_SAMPLES_LEAF = 1
 
 RANDOM_SEED = 42
 
