de855fd39b387db5657ecd9e0d48144b0d3e8758
cemd <99793829+cemdusenkalkan@users.noreply.github.com>
2023-12-05 20:05:28 +0300

    Update data_handler.py

--- src/data_handler.py
+++ src/data_handler.py
@@ -11,21 +11,29 @@ return will be dataframe.(loaded data)
 
 import pandas as pd
 
+
 def load_data(file_path):
     try:
         data = pd.read_csv(file_path)
-        print("loading is succesfull.")
         return data
     except FileNotFoundError:
         print(f"File not found: {file_path}")
         return None
 
+
 def preprocess_data(data):
     if 'Date' in data.columns:
         data['Date'] = pd.to_datetime(data['Date'])
-        data.set_index('Date',inplace=True)
-        return data
+        data.set_index('Date', inplace=True)
 
-#Now, this is a skeleton for sure, not handling
-#the missing values, normalizing, exceptions etc.
+        data = data.dropna()
+        print("Checking for NaN values in the DataFrame:")
+        print(data.isnull().sum())
 
+    return data
+
+
+""""
+Now, this is a skeleton for sure, not handling
+the missing values, normalizing, exceptions etc
+"""""
